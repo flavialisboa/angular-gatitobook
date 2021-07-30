d5d23c58470d72ddf3314a446fafe0a666feb5c6
Flavia Lisboa <flavialisboa5@gmail.com>
2021-07-30 08:24:40 -0300

    Adição de repositório

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,24 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+import { CabecalhoModule } from './componentes/cabecalho/cabecalho.module';
+import { RodapeModule } from './componentes/rodape/rodape.module';
+import { AutenticacaoModule } from './autenticacao/autenticacao.module';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    CabecalhoModule,
+    RodapeModule,
+    AutenticacaoModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/componentes/cabecalho/cabecalho.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CabecalhoComponent } from './cabecalho.component';
+import { RouterModule } from '@angular/router';
+import { MenuModule } from '../menu/menu.module';
+
+
+
+@NgModule({
+  declarations: [
+    CabecalhoComponent
+  ],
+  imports: [
+    CommonModule,
+    RouterModule,
+    MenuModule
+  ],
+  exports: [
+    CabecalhoComponent
+  ]
+})
+export class CabecalhoModule { }
